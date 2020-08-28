cc4eaee0733cdd301fa8a6420d424367f40855be
Samuel Duursma <sd.duursma@gmail.com>
2020-08-28 08:56:39 +0200

    Add emitln and emits
    Which for now are just aliases of println and print respectively.

--- src/main/clojure/cljn/emitter.clj
+++ src/main/clojure/cljn/emitter.clj
@@ -5,21 +5,25 @@
 (defn emit [ast frame]
   (-emit ast frame))
 
+(def emitln println)
+
+(def emits print)
+
 (defmethod -emit :const
   [{:keys [val env :as ast]} frame]
   (let [context (:context env)]
     (when-not (isa? context :ctx/statement)
       (case val
         nil (do (when (isa? context :ctx/return)
-                  (print "return "))
-                (print "nil"))
+                  (emits "return "))
+                (emits "nil"))
         ;; TODO: Handle other constants as well.
         ))))
 
 (defmethod -emit :do
   [{:keys [statements ret env]} frame]
   (let [context (:context env)]
-    (when (and (seq statements) (isa? context :ctx/expr)) (println "({ () -> Any? in"))
+    (when (and (seq statements) (isa? context :ctx/expr)) (emitln "({ () -> Any? in"))
     (doseq [s statements] (emit s frame))
     (emit ret frame)
-    (when (and (seq statements) (= :expr context)) (println "})()"))))
+    (when (and (seq statements) (= :expr context)) (emitln "})()"))))
