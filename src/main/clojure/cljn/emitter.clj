741f219a20453c3f35501b335012eccd4f3c6b26
Samuel Duursma <sd.duursma@gmail.com>
2020-08-28 10:32:35 +0200

    Emit multimethod and emit bool and UUID

--- src/main/clojure/cljn/emitter.clj
+++ src/main/clojure/cljn/emitter.clj
@@ -9,16 +9,41 @@
 
 (def emits print)
 
+(defmacro emit-contextually
+  "Macro that wraps its body with, for example, 'return' and ';', depending on the context."
+  [env & body]
+  `(let [env# ~env]
+     (when (isa? (:context env#) :ctx/return) (emits "return "))
+     ~@body
+     (when-not (= (:context env#) :ctx/expr) (emitln ";"))))
+
+;; Why class instead of type?
+(defmulti emit-constant* class)
+
+(defmethod emit-constant* nil [_]
+  (emits "nil"))
+
+(defmethod emit-constant* Boolean [x]
+  (emits (if x "true" "false")))
+
+(defmethod emit-constant* java.util.UUID [^java.util.UUID uuid]
+  ;; Force unwrap is OK because we know uuid is actually a UUID.
+  ;; Use different UUID implementation because Foundation.UUID returns uppercase strings?
+  (emits (str "Foundation.UUID(uuidString: \"" (.toString uuid) "\")!")))
+
+(defn emit-constant-no-meta [v]
+  ;; TODO: Emit lists, vectors, maps, etc.
+  (emit-constant* v))
+
+(defn emit-constant [v]
+  ;; TODO: Elide metadata?
+  (emit-constant-no-meta v))
+
 (defmethod -emit :const
-  [{:keys [val env :as ast]} frame]
+  [{:keys [env form]} frame]
   (let [context (:context env)]
     (when-not (isa? context :ctx/statement)
-      (case val
-        nil (do (when (isa? context :ctx/return)
-                  (emits "return "))
-                (emits "nil"))
-        ;; TODO: Handle other constants as well.
-        ))))
+      (emit-contextually env (emit-constant form)))))
 
 (defmethod -emit :do
   [{:keys [statements ret env]} frame]
