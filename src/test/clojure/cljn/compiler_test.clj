7ab9315a7adabd42fcbeebfb3b9c1d8b464435a6
Samuel Duursma <sd.duursma@gmail.com>
2020-12-06 14:53:34 +0100

    Add compiler-test ns with basic test

---
+++ src/test/clojure/cljn/compiler_test.clj
@@ -0,0 +1,10 @@
+(ns cljn.compiler-test
+  (:require [clojure.test :refer :all]
+            [cljn.compiler :as c]))
+
+(deftest test-compiler
+  (let [uuid #uuid "1369709c-2bdc-4e35-9ae1-1cde9068f672"]
+    (is
+      (= (with-out-str
+           (c/compile '#uuid "1369709c-2bdc-4e35-9ae1-1cde9068f672" {:context :ctx/return}))
+         (str "return Foundation.UUID(uuidString: \"" (.toString uuid) "\")!;\n")))))
